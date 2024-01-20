3ffc21a933c0ec418994c5b7767bdf5e031b6bef
Pratik <pratik.sonawane@valamis.com>
2024-01-20 23:28:57 +0530

    added yml to connect with docker

--- prisma/migrations/20240120100107_comment_count_type_change_from_big_int_to_int/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `comment_count` on the `Post` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Int`.
-
-*/
--- AlterTable
-ALTER TABLE `Post` MODIFY `comment_count` INTEGER NOT NULL DEFAULT 0;
